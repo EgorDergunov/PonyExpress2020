67f4bd75be18a3b36f0a6e107a00039333f3e10f
EgorDergunov <egor.dd3@ya.ru>
2020-10-02 17:02:53 +0400

    Update list.py

--- list.py
+++ list.py
@@ -5,3 +5,11 @@ def list_01 (l):
           if l[i]=l[j]:
              b='Yes'
    return (b)
+
+class rectangle:
+   def _init_(self, list_of_dots = []):
+      self.dots = list_of_dots
+   def square(self):
+      return (self.dots[2,1]-self.dots[1,1])*(self.dots[2,2]-self.dots[1,2])
+   def perimetr(self):
+      return 2*(self.dots[2, 1] - self.dots[1, 1]) + (self.dots[2, 2] - self.dots[1, 2])
