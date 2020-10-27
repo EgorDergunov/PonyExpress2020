ea62b851c8680d6231a1fab1fe213b1bfb6fd36d
EgorDergunov <egor.dd3@ya.ru>
2020-10-27 15:39:20 +0400

    Create pathes.py

---
+++ pathes.py
@@ -0,0 +1,7 @@
+login = 'ext.mgu_education'
+password = 'rg#P5hZm4F'
+
+driverpath = r'C:\Users\Дергунов Денис\Desktop\geckodriver.exe'
+pegas_url = 'http://pegasus-edu.pegasus.ponyex.local/'
+
+browser = 'Firefox'
